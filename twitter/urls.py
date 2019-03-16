a60e12c086fd7519dfeb9a490ae33ee5774baeff
kaminskij <jakub.kamin2@gmail.com>
2019-03-16 15:28:27 +0100

    updated urls

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views import generic
+from twitter.views import MainPageView
 
 
 app_name = 'twitter'  # tutaj dajac app name wzystkie nazwy jak index musza byc poprzedzana nazwa aplikacji(patrz heml
 # tak unikniemy nadpisywania nazw
 
 urlpatterns = [
-   path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
-        name='index'),  # dajemu tu generyczny template view bo nie ma tu nic specjalnego. Dajemy template name i sie o
+   path('', MainPageView.as_view(template_name='twitter/index.html'),
+        name='index'),
 ]
