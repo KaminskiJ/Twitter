5ab647d9e9f7a3c04203d2fb6a39996727dad558
kaminskij <jakub.kamin2@gmail.com>
2019-03-16 15:27:38 +0100

    main page view

--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views import View, generic
+from twitter.models import Tweet
 # Create your views here.
+
+
+# widok ktory czyta wszystkie tweety, szereguje je po dacie i wrzuca do ctx
+class MainPageView(generic.ListView):
+
+    template_name = 'twitter/index.html'
+    context_object_name = 'tweets'
+
+    def get_queryset(self):
+        return Tweet.objects.all().order_by('-creation_date')
