21921e8fc3785d32411f955b6f8b22ea9acc3d84
kaminskij <jakub.kamin2@gmail.com>
2019-03-16 16:35:01 +0100

    update

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,12 +1,12 @@
+from django import views
 from django.urls import path
-from django.views import generic
-from twitter.views import MainPageView
+from twitter.views import MainPageView, RegisterView
 
 
 app_name = 'twitter'  # tutaj dajac app name wzystkie nazwy jak index musza byc poprzedzana nazwa aplikacji(patrz heml
 # tak unikniemy nadpisywania nazw
 
 urlpatterns = [
-   path('', MainPageView.as_view(template_name='twitter/index.html'),
-        name='index'),
+    path('', MainPageView.as_view(template_name='twitter/index.html'), name='index'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View, generic
 from twitter.models import Tweet
+from django import forms
+from twitter.forms import UserRegisterForm
+
 # Create your views here.
 
 
@@ -12,3 +15,23 @@ class MainPageView(generic.ListView):
 
     def get_queryset(self):
         return Tweet.objects.all().order_by('-creation_date')
+
+
+class RegisterView(View):
+    form_class = UserRegisterForm
+    template_name = 'twitter/register.html'
+
+    def get(self, request):
+        return render(request, 'twitter/register.html',
+                     {'form': UserRegisterForm()})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+            return redirect('/')
+
+        return render(request, self.template_name, {'form': form})
